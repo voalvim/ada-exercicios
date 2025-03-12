34b007b86319fb252d229c32e3b21b16c1371373
Vitor Ornelas Alvim <vitor.alvim@caixa.gov.br>
2025-03-12 18:15:19 -0300

    Remoção de pacotes desnecessários

--- src/main/java/aula5/exercicio2/NotificacaoEmail.java
+++
@@ -1,8 +0,0 @@
-package aula5.exercicio2;
-
-public class NotificacaoEmail implements Notificacao {
-    @Override
-    public void enviar(String destinatario, String mensagem) {
-        System.out.println("Enviando email para " + destinatario + ": " + mensagem);
-    }
-}
--- src/main/java/aula5/exercicio2/NotificacaoSMS.java
+++
@@ -1,8 +0,0 @@
-package aula5.exercicio2;
-
-public class NotificacaoSMS implements Notificacao {
-    @Override
-    public void enviar(String destinatario, String mensagem) {
-        System.out.println("Enviando SMS para " + destinatario + ": " + mensagem);
-    }
-}
